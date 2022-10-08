17cf35da19765d6b1a2ce155c07dd2c5033f6a8c
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2022-10-09 02:01:24 +0530

    Backend Structuring

--- bezen/app/views.py
+++ bezen/app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from app.models import recipe
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class Index(TemplateView):
+    context = {}
+    template_name = 'app/index.html'
+
+    def get(self, request, *args, **kwargs):
+        user = request.user.id
+        if user:
+            return redirect('index')
+        data = recipe.objects.all()
+        self.context['data'] = data
+        return render(request,self.template_name , self.context)
+
